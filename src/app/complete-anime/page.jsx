5bbeb9cedf2a4f39dafa538e96ab7abc8b403262
Cxxnd <bwong0227@gmail.com>
2026-01-02 10:02:55 +0700

    .

---
+++ src/app/complete-anime/page.jsx
@@ -0,0 +1,79 @@
+"use client";
+
+import AnimeList from "@/components/AnimeList";
+import Pagination from "@/components/Utilities/Pagination";
+import ButtonBack from "@/components/Navbar/ButtonBack";
+import { useState, useEffect } from "react";
+import { useSearchParams } from "next/navigation";
+import { getAnime } from "@/libs/service-api";
+
+export default function Complete() {
+    const searchParams = useSearchParams();
+    const currentPage = Number(searchParams.get("page")) || 1;
+
+    const [page, setPage] = useState(currentPage);
+    const [animeData, setAnimeData] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            setLoading(true);
+            try {
+                const res = await getAnime({
+                    resource: `complete-anime?page=${page}`,
+                });
+                setAnimeData(res);
+            } catch (err) {
+                console.error(err);
+                setAnimeData(null);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchData();
+    }, [page]);
+
+    const list = animeData?.data?.animeList || [];
+    const pagination = animeData?.pagination;
+
+    return (
+        <div>
+            {loading ? (
+                <div className="text-center py-20 text-gray-400">
+                    Sedang memuat data anime...
+                </div>
+            ) : list.length === 0 ? (
+                <div className="text-center py-20 text-gray-400">
+                    Tidak ada anime yang ditemukan.
+                </div>
+            ) : (
+                <div className="p-4 max-w-7xl mx-auto">
+                    <ButtonBack />
+                    {/* LIST */}
+                    <AnimeList api={list} />
+
+                    {/* PAGINATION */}
+                    <div className="flex justify-center items-center gap-2 py-4 flex-col bg-gray-900 text-white">
+                        <p className="text-gray-400">
+                            Halaman{" "}
+                            <span className="text-white font-semibold">
+                                {pagination.currentPage}
+                            </span>{" "}
+                            dari{" "}
+                            <span className="text-white font-semibold">
+                                {pagination.totalPages}
+                            </span>
+                        </p>
+
+                        <Pagination
+                            page={page}
+                            lastPage={pagination.totalPages}
+                            setPage={setPage}
+                        />
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+}
---
+++ src/app/episode/[slug]/page.jsx
@@ -0,0 +1,125 @@
+import { getAnime } from "@/libs/service-api";
+import Link from "next/link";
+import VideoPlayer from "@/components/Player/VideoPlayer";
+import ButtonBack from "@/components/Navbar/ButtonBack";
+
+const Page = async ({ params }) => {
+    const { slug } = await params;
+    const episode = await getAnime({ resource: `episode/${slug}` });
+
+    const data = episode?.data;
+    const downloads = data?.downloadUrl;
+
+    return (
+        <div className="min-h-screen bg-gradient-to-b from-gray-950 via-gray-900 to-gray-950 text-white px-6 py-10 space-y-20">
+            <div className="max-w-6xl mx-auto space-y-10">
+                <div className="flex justify-between items-center p-2">
+                    <ButtonBack />
+                    <h2 className="text-2xl font-bold text-center">
+                        {data.title}
+                    </h2>
+                </div>
+                <div className="bg-black text-white p-6 rounded-xl">
+                    {/* PLAYER */}
+                    <div className="my-30">
+                        <VideoPlayer streamServers={data.server.qualities} />
+
+                        {/* Navigasi Episode */}
+                        <div className="flex justify-between align-items-center mt-15">
+                            {data.prevEpisode && (
+                                <Link
+                                    href={`${data.prevEpisode.href}`}
+                                    className="px-4 py-2 bg-blue-600 rounded"
+                                >
+                                    ⬅ Sebelumnya
+                                </Link>
+                            )}
+
+                            {data.nextEpisode && (
+                                <Link
+                                    href={`${data.nextEpisode.href}`}
+                                    className="px-4 py-2 bg-blue-600 rounded"
+                                >
+                                    Selanjutnya ➡
+                                </Link>
+                            )}
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {/* Info Anime */}
+            <div className="bg-gray-900/50 p-8 rounded-2xl border border-gray-800">
+                <h2 className="text-2xl font-bold text-blue-400 mb-4">
+                    🎬 Info Anime
+                </h2>
+                <p className="text-gray-300 mb-3">
+                    <span className="font-semibold text-white">Judul:</span>{" "}
+                    {(() => {
+                        try {
+                            const parts = data.title.split("/");
+                            const lastPart = parts.filter(Boolean).pop();
+                            return lastPart.replace(/-/g, " ");
+                        } catch {
+                            return "Tidak diketahui";
+                        }
+                    })()}
+                </p>
+                <p className="text-gray-300">
+                    <span className="font-semibold text-white">Direlease:</span>{" "}
+                    {data.releaseTime}
+                </p>
+                <p className="text-gray-300">
+                    <span className="font-semibold text-white">Credit:</span>{" "}
+                    {data.info.credit || "Tidak diketahui"}
+                </p>
+            </div>
+
+            {/* Tombol Download */}
+            <div className="space-y-10">
+                <h2 className="text-2xl font-bold text-blue-400">
+                    💾 Download Links
+                </h2>
+
+                {Object.entries(downloads).map(([format, list]) => (
+                    <div key={format}>
+                        <h3 className="text-xl capitalize mb-4 font-semibold text-gray-200">
+                            Format {format}
+                        </h3>
+
+                        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-6">
+                            {list.map((res, resIndex) => (
+                                <div
+                                    key={`${format}-${res.resolution}-${resIndex}`}
+                                    className="bg-gray-800 p-4 rounded-xl border border-gray-700"
+                                >
+                                    <h2 className="text-lg font-bold text-blue-300 mb-2">
+                                        {res.title}
+                                    </h2>
+                                    <h4 className="text-lg font-bold text-blue-300 mb-2">
+                                        {res.size}
+                                    </h4>
+
+                                    <div className="space-y-2">
+                                        {res.urls.map((link, i) => (
+                                            <a
+                                                key={i}
+                                                href={link.url}
+                                                target="_blank"
+                                                className="block bg-blue-600 hover:bg-blue-700 px-3 py-2 rounded-md text-sm text-center transition"
+                                            >
+                                                {link.title}
+                                            </a>
+                                        ))}
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default Page;
