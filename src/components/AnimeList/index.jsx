e677a8a543a2c4d3ce6add49db903915791b4b76
Cxxnd <bwong0227@gmail.com>
2026-03-07 00:13:09 +0700

    fix again

--- src/components/AnimeList/index.jsx
+++ src/components/AnimeList/index.jsx
@@ -37,7 +37,7 @@ const AnimeList = ({ api, mode }) => {
     }
 
     return (
-        <div className="grid gap-6 [grid-template-columns:repeat(auto-fill,300px)] justify-center bg-gray-900 min-h-screen p-4">
+        <div className="grid gap-6 [grid-template-columns:repeat(auto-fill,220px)] justify-center bg-gray-900 min-h-screen p-4">
             {animeData?.map((anime, index) => (
                 <Link
                     href={`/anime/${anime.animeId}`}
