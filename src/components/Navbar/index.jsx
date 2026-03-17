87283cc6beb7f88bd8e8fef16bf0ac5689d443af
Cxxnd <bwong0227@gmail.com>
2026-03-17 14:06:11 +0700

    fix

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -3,13 +3,13 @@ import UserActivation from "./UserActivation";
 
 const Navbar = () => {
     return (
-        <header className="bg-gradient-to-r from-cyan-400 via-cyan-400 to-blue-600 shadow-lg z-50 rounded-2xl w-full items-center justify-center">
-            <div className="max-w-7xl mx-96 px-4">
+        <header className="bg-gradient-to-r from-cyan-400 via-cyan-400 to-blue-600 shadow-lg rounded-2xl w-full">
+            <nav className="max-w-7xl mx-auto px-4 items-center">
                 <div className="flex flex-col md:flex-row items-center justify-between gap-4 py-4">
                     {/* Logo */}
                     <Link
                         href="/"
-                        className="font-extrabold text-2xl text-white hover:text-gray-300 transition-colors duration-300"
+                        className="font-extrabold text-2xl text-white whitespace-nowrap"
                     >
                         LAZARUSNIME
                     </Link>
@@ -17,7 +17,7 @@ const Navbar = () => {
                     {/* User + Search */}
                     <UserActivation />
                 </div>
-            </div>
+            </nav>
         </header>
     );
 };
