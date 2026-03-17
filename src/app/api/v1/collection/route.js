c0a8875d40c22fb3c3adbb80bb607dee9d1192a4
Cxxnd <bwong0227@gmail.com>
2026-03-17 09:23:26 +0700

    M

--- src/app/api/v1/collection/route.js
+++ src/app/api/v1/collection/route.js
@@ -1,22 +1,7 @@
-import prisma from "@/libs/prisma";
-
-export default async function GET(request) {
-    try {
-        const { slug, poster, title, user_email, provider } = await request.json();
-        const data = {
-            slug,
-            poster,
-            title,
-            user_email,
-            provider
-        }
-        const collection = await prisma.Collection.create({ data })
-        return Response.json(collection, { status: 200 })
-    } catch (error) {
-        console.error('Prisma error:', error)
-        return Response.json(
-            { error: 'Internal Server Error', details: error.message },
-            { status: 500 }
-        )
-    }
+export default page = () => {
+    return (
+        <div>
+            <h1>Collection</h1>
+        </div>
+    )
 }
