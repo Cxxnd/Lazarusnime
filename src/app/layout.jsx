f6b8ba8e357eb3eba0e74501cd148f13a05535a4
Cxxnd <bwong0227@gmail.com>
2026-06-16 22:12:50 +0700

    update icon2

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -17,6 +17,8 @@ const geistMono = Geist_Mono({
 
 export const metadata = {
     title: "LazarusNime",
+    icon: "/favicon.ico",
+    shortcut: "/favicon.ico",
     description: "Stream Anime Gratis dan Cepat",
 };
 
